d6d696f1063af1dd61987d7bf7105de413627285
devashish bakare <devashishbakare@gmail.com>
2023-04-07 18:00:03 +0530

    added feed and post jsx and css to it

--- frontend/src/components/Feed/Feed.jsx
+++ frontend/src/components/Feed/Feed.jsx
@@ -1,7 +1,46 @@
 import React from "react";
-
+import style from "./feed.module.css";
+import profilePicture from "../../assets/user/img2.jpg";
+import { FcAddImage, FcLike } from "react-icons/fc";
+import Post from "../Post/Post";
 const Feed = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className={style.uploadPostContainer}>
+        <div className={style.profileAndDesription}>
+          <img
+            src={profilePicture}
+            alt="profile img"
+            className={style.uploadPostProfilePhoto}
+          />
+          <input
+            type="text"
+            className={style.uploadPostDescription}
+            placeholder="whats in your mind Rehana..."
+          />
+        </div>
+        <hr />
+        <div className="uploadSharingDetails">
+          <ul className={style.listItmes}>
+            <li>
+              <FcAddImage className={style.iconStyle} />
+              <span className={style.uploadPostText}> Share Photo</span>
+            </li>
+            <li>
+              <FcLike className={style.iconStyle} />
+              <span className={style.uploadPostText}> Feelings</span>
+            </li>
+            <li>
+              <button className={style.uploadShareButton}> Share</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={style.uploadedPost}>
+        <Post />
+      </div>
+    </>
+  );
 };
 
 export default Feed;
---
+++ frontend/src/components/Post/Post.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import style from "./post.module.css";
+import { FaEllipsisV } from "react-icons/fa";
+import userImage from "../../assets/user/img1.jpg";
+import postImage from "../../assets/post/post1.jpeg";
+import like from "../../assets/like.jpeg";
+import love from "../../assets/love.jpeg";
+const Post = () => {
+  return (
+    <div className={style.postContainer}>
+      <div className={style.postTopDetails}>
+        <div className={style.postTopDetailsInfo}>
+          <img className={style.postUserImg} src={userImage} alt="user_image" />
+          <span className={style.postUserName}>Tylor Swift</span>
+          <span className={style.postTiming}>5 min ago</span>
+        </div>
+        <div className="postHeadingIcon">
+          <FaEllipsisV className={style.postHeadingInfoIcon} />
+        </div>
+      </div>
+      <div className={style.postDescImg}>
+        <div className={style.postDesc}>
+          <span className={style.descText}>
+            We have to prove ourself on each path...
+          </span>
+        </div>
+        <div className={style.postImgDiv}>
+          <img className={style.postImg} src={postImage} alt="Post_image" />
+        </div>
+      </div>
+      <div className={style.postBottomWrapper}>
+        <div className={style.postButtomImgWrapper}>
+          <img src={like} alt="Like_Image" className={style.postLikeImg} />
+          <img src={love} alt="Love_Image" className={style.postLoveImg} />
+        </div>
+        <div className={style.postCommnetWrapper}>
+          {/* Todo : we have to use link in router-dom */}
+          <a href="" className="commentLink">
+            Comments
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
